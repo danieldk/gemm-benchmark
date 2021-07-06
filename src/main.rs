2f9a9587af97c92e48b833d601e66d578c6d8a42
Daniël de Kok <me@danieldk.eu>
2021-07-06 19:57:25 +0200

    Add basic [sd]gemm benchmark
    This is similar to the ACES dgemm benchmark, but does not require
    hacking to use modern BLAS libraries or benchmark sgemm.

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+#[cfg(feature = "intel-mkl-amd")]
+use std::os::raw::c_int;
+use std::time::Duration;
+
+use clap::{AppSettings, Clap};
+use ndarray::linalg::general_mat_mul;
+use ndarray::{Array2, LinalgScalar};
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::ParallelIterator;
+
+#[cfg(feature = "accelerate")]
+extern crate accelerate_src;
+
+#[cfg(any(feature = "intel-mkl", feature = "intel-mkl-amd"))]
+extern crate intel_mkl_src;
+
+#[cfg(feature = "openblas")]
+extern crate openblas_src;
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Matrix dimensionality (N x N).
+    #[clap(short, long, default_value = "256")]
+    dim: usize,
+
+    /// Double precision (DGEMM).
+    #[clap(long)]
+    dgemm: bool,
+
+    /// The number of gemm iterations per thread.
+    #[clap(short, long, default_value = "1000")]
+    iterations: usize,
+
+    /// The number of benchmark threads.
+    #[clap(short, long, default_value = "1")]
+    threads: usize,
+}
+
+struct BenchmarkStats {
+    elapsed: Duration,
+    flops: usize,
+}
+
+fn gemm_benchmark<A>(dim: usize, iterations: usize, threads: usize) -> BenchmarkStats
+where
+    A: LinalgScalar + Send + Sync,
+{
+    let one = A::one();
+    let two = one + one;
+    let point_five = one / two;
+
+    let matrix_a: Array2<A> = Array2::from_elem((dim, dim), two);
+    let matrix_b: Array2<A> = Array2::from_elem((dim, dim), point_five);
+    let c_matrices: Vec<_> = std::iter::repeat(Array2::from_elem((dim, dim), one))
+        .take(threads)
+        .collect::<Vec<_>>();
+
+    let start = std::time::Instant::now();
+
+    c_matrices.into_par_iter().for_each(|mut matrix_c| {
+        for _ in 0..iterations {
+            general_mat_mul(A::one(), &matrix_a, &matrix_b, A::one(), &mut matrix_c);
+        }
+    });
+
+    let elapsed = start.elapsed();
+
+    BenchmarkStats {
+        elapsed,
+        flops: (dim.pow(3) * 2 * iterations * threads) + (dim.pow(2) * 2 * iterations * threads),
+    }
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(opts.threads)
+        .build_global()
+        .unwrap();
+
+    eprintln!("Threads: {}", opts.threads);
+    eprintln!("Iterations per thread: {}", opts.iterations);
+    eprintln!("Matrix shape: {} x {}", opts.dim, opts.dim);
+
+    let stats = if opts.dgemm {
+        gemm_benchmark::<f64>(opts.dim, opts.iterations, opts.threads)
+    } else {
+        gemm_benchmark::<f32>(opts.dim, opts.iterations, opts.threads)
+    };
+
+    eprintln!(
+        "GFLOPS/s: {:.2}",
+        (stats.flops as f64 / stats.elapsed.as_secs_f64()) / 1000_000_000.
+    );
+}
+
+#[cfg(feature = "intel-mkl-amd")]
+#[allow(dead_code)]
+#[no_mangle]
+extern "C" fn mkl_serv_intel_cpu_true() -> c_int {
+    1
+}
