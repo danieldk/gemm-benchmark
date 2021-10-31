09034676fd56c4d8bf28ee2e4169b40a43a67bbc
Daniël de Kok <me@danieldk.eu>
2021-10-31 10:46:08 +0100

    Criterion experiment WIP

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::os::raw::c_int;
 use std::time::Duration;
 
+use criterion::measurement::{Measurement, WallTime};
+use criterion::{BenchmarkGroup, Criterion, Throughput};
 use ndarray::linalg::general_mat_mul;
 use ndarray::{Array2, LinalgScalar};
 use rayon::iter::IntoParallelIterator;
@@ -40,38 +42,28 @@ struct Opts {
     threads: usize,
 }
 
-struct BenchmarkStats {
-    elapsed: Duration,
-    flops: usize,
+fn dim_to_flop(dim: usize) -> usize {
+    (dim.pow(3) * 2) + (dim.pow(2) * 2)
 }
 
-fn gemm_benchmark<A>(dim: usize, iterations: usize, threads: usize) -> BenchmarkStats
+fn gemm_benchmark<A, M>(mut group: BenchmarkGroup<M>)
 where
     A: LinalgScalar + Send + Sync,
+    M: Measurement,
 {
     let one = A::one();
     let two = one + one;
     let point_five = one / two;
 
-    let matrix_a: Array2<A> = Array2::from_elem((dim, dim), two);
-    let matrix_b: Array2<A> = Array2::from_elem((dim, dim), point_five);
-    let c_matrices: Vec<_> = std::iter::repeat(Array2::from_elem((dim, dim), one))
-        .take(threads)
-        .collect::<Vec<_>>();
+    for dim in [128, 256, 384, 512, 768, 1024, 1152, 1280, 1408, 2048] {
+        let matrix_a: Array2<A> = Array2::from_elem((dim, dim), two);
+        let matrix_b: Array2<A> = Array2::from_elem((dim, dim), point_five);
+        let mut matrix_c = Array2::from_elem((dim, dim), one);
 
-    let start = std::time::Instant::now();
-
-    c_matrices.into_par_iter().for_each(|mut matrix_c| {
-        for _ in 0..iterations {
-            general_mat_mul(A::one(), &matrix_a, &matrix_b, A::one(), &mut matrix_c);
-        }
-    });
-
-    let elapsed = start.elapsed();
-
-    BenchmarkStats {
-        elapsed,
-        flops: (dim.pow(3) * 2 * iterations * threads) + (dim.pow(2) * 2 * iterations * threads),
+        group.throughput(Throughput::Elements(dim_to_flop(dim) as u64));
+        group.bench_function(format!("{}x{}", dim, dim), |b| {
+            b.iter(|| general_mat_mul(A::one(), &matrix_a, &matrix_b, A::one(), &mut matrix_c))
+        });
     }
 }
 
@@ -87,16 +79,10 @@ fn main() {
     println!("Iterations per thread: {}", opts.iterations);
     println!("Matrix shape: {} x {}", opts.dim, opts.dim);
 
-    let stats = if opts.dgemm {
-        gemm_benchmark::<f64>(opts.dim, opts.iterations, opts.threads)
-    } else {
-        gemm_benchmark::<f32>(opts.dim, opts.iterations, opts.threads)
-    };
+    let mut criterion = Criterion::default();
 
-    println!(
-        "GFLOPS/s: {:.2}",
-        (stats.flops as f64 / stats.elapsed.as_secs_f64()) / 1000_000_000.
-    );
+    gemm_benchmark::<f32, WallTime>(criterion.benchmark_group("sgemm"));
+    gemm_benchmark::<f64, WallTime>(criterion.benchmark_group("dgemm"));
 }
 
 #[cfg(feature = "intel-mkl-amd")]
